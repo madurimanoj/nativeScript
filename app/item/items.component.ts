7beb84b5a8478d13078c015c9256cde6d2116a47
Manoj <manoj@Turtle-Jr-II.local>
2018-01-11 17:27:01 -0500

    added images and some markup as login

--- app/item/items.component.ts
+++ app/item/items.component.ts
@@ -7,15 +7,30 @@ import { ItemService } from "./item.service";
     selector: "ns-items",
     moduleId: __filename,
     templateUrl: "./items.component.html",
+    styleUrls: ["./items.component.css"]
 })
 export class ItemsComponent implements OnInit {
     items: Item[];
+    page;
+    i;
 
     // This pattern makes use of Angular’s dependency injection implementation to inject an instance of the ItemService service into this class. 
     // Angular knows about this service because it is included in your app’s main NgModule, defined in app.module.ts.
     constructor(private itemService: ItemService) { }
 
     ngOnInit(): void {
-        this.items = this.itemService.getItems();
+        // this.items = this.itemService.getItems();
+        // this.page.actionBarHidden = true;
+        this.page.backgroundImage = "res://bg_login";
     }
+
+    email = "nativescriptrocks@telerik.com";
+  isLoggingIn = true;
+
+  submit() {
+    alert("You’re using: " + this.email);
+  }
+  toggleDisplay() {
+    this.isLoggingIn = !this.isLoggingIn;
+  }
 }
